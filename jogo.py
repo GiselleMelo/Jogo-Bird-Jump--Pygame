833d1cb9dece3f316c5579799aa162383e8b7e72
GiselleMelo <gisellevieira4@gmail.com>
2021-05-31 11:49:43 -0300

    adicionando os sons e implementando o método de eventos

--- jogo.py
+++ jogo.py
@@ -65,7 +65,9 @@ class Jogo:
             while self.game:
                 self.jog_inicial()
                 while self.state == 'TRANSIÇÃO':
-                    pass
+                    self.trans_eventos()
+                    self.trans_draw()
+                    self.clock.tick(fps)
                 while self.state == 'JOGANDO':
                     self.jog_eventos()
                     self.jog_draw()
@@ -112,3 +114,94 @@ class Jogo:
     def ini_draw(self):
         pygame.display.update()
         pygame.Surface.fill(self.window,(255,255,255))
+
+######################################## JOGANDO ####################################################
+#Nessa parte o jogo será iniciado para a interação com o jogador
+    
+    def jog_inicial(self):
+        
+        # Inicializando sprites/grupos
+        self.bird = Bird(self.jogador_selecionado)
+        self.all_pipes = pygame.sprite.Group()
+        self.all_birds = pygame.sprite.Group()
+        self.all_birds.add(self.bird)
+        
+        # Impede de segurar o botão para voar constantemente
+        self.apertado = False
+        
+        # Posição e velocidade do chão
+        self.ground_scroll = 0
+        self.ground_vel = - 4
+        
+        # Impede a movimentação do chão e a criação de canos
+        self.bateu = False
+        self.last_pipe = pygame.time.get_ticks()
+
+        # Impede while da condição de game over rodar o timer de novo:
+        self.acabou = False 
+        
+        # Variáveis para formar o placar. Self.passou verifica se o pássaro está dentro do cano, para evitar contar mais de um ponto por cano
+        self.score = 0
+        self.passou = False
+
+
+        # Variável que auxilia a passagem de nível
+        self.momento_passa_nível = pygame.time.get_ticks()
+        self.passa_nível = True
+    
+
+    def jog_eventos(self):
+        
+        # Novos canos:
+
+        #armazenando o tempo que está passando 
+        now = pygame.time.get_ticks()
+
+        #implementando as condições para gerar novos canos
+        if now - self.last_pipe > self.bird.freq and self.bird.voando == True and self.bateu == False:
+            self.last_pipe = now
+            baixo = Pipe('baixo',self.ground_vel)
+            cima = Pipe('topo',self.ground_vel)
+            #garantindo que exista um espaço fixo entre os canos
+            baixo.rect.top = cima.rect.bottom + baixo.gap
+            #adicionando os canos:
+            self.all_pipes.add(baixo)
+            self.all_pipes.add(cima)
+            
+        
+        # Rolamento do chão 
+        #confere se o jogador bateu
+        if self.bateu == False:
+            self.ground_scroll += self.ground_vel
+            #recoloca o chão dando o efeito de continuidade
+            if abs(self.ground_scroll) > 35:
+                self.ground_scroll = 0
+            
+        # Condição de game over:
+        #checa se há colisão entre os sprites (pássaro e cano)
+        hit =  pygame.sprite.groupcollide(self.all_birds,self.all_pipes,False,False)
+        
+        #checa se ele cai no chão ou bate no cano
+        if self.bird.rect.bottom >= g_HEIGHT or hit:
+            #percorre a lista de canos e faz todos eles pararem.
+            for sprite in self.all_pipes.sprites():
+                sprite.para()
+            #atualiza as condições
+            self.bateu = True
+            self.bird.pode_pular = False
+            self.ground_vel = 0
+            #garantindo que o self.momento não rode de novo
+            if self.acabou == False:
+                self.acabou = True
+                #adicionando o som
+                self.assets['morte'].play()
+                #guardando o momento
+                self.momento = pygame.time.get_ticks()
+            else:    
+                #checa se passou dois segundo após o self.momento para dar a condição de fim.
+                if (now - self.momento > 2000):
+                    self.state = "FIM"
+
+        
+         
+        
