2790f4f3242cf1f37a9af1d61611913b0d8fa048
NatanLewi <natan.lewi@terra.com.br>
2021-06-06 21:09:23 -0300

    Importando e carregando os backgrounds

--- configuração.py
+++ configuração.py
@@ -18,10 +18,20 @@ b_ini = (HEIGHT/2) - 100
 #Função que inicia os assets(será chamada pela classe do jogo)
 def load_assets():
     assets = {}
-    assets['bg'] = pygame.image.load('').convert()
-    #transformando para a escala do jogo
-    assets['bg'] = pygame.transform.scale(assets['bg'],(WIDTH,HEIGHT))
-    assets['ground'] = pygame.image.load('').convert()
+    assets['bg'] = []
+    
+    #carregando os backgrounds png na lista
+    for i in range(1,5):
+        img = pygame.image.load('imagens/bg_{0}.png'.format(i)).convert()
+        #transformando a escala da imagem
+        img =  pygame.transform.scale(img,(WIDTH,HEIGHT))
+        assets['bg'].append(img)
+    #carregando o background jpg na lista
+    img2 = pygame.image.load('imagens/background.jpg').convert()
+    #transformando a escala da imagem
+    img2 = pygame.transform.scale(img2,(WIDTH,HEIGHT))
+    assets['bg'].append(img2)
+    assets['ground'] = pygame.image.load('imagens/ground.png').convert()
     # Importanto fontes
     assets['fonte'] = pygame.font.SysFont('Bauhaus 93',40)
     assets['fonte2'] = pygame.font.SysFont('Bauhaus 93',20)
